2005228b525ec37a5db8bacea76c16314ccee20d
Kevin Ip <ipkevin@gmail.com>
2023-04-02 15:30:29 -0700

    add initial setup logic for game state (deck, hands, bets, bank) and create bet and hit moves. Added initial phases for betting and playing

--- bgio-tutorial/src/App.js
+++ bgio-tutorial/src/App.js
@@ -2,14 +2,14 @@ import { Client } from 'boardgame.io/react';
 import {TicTacToe} from './Game';
 import {TicTacToeBoard} from './Board';
 
-// I THINK this is related to old style React components built by class vs built by functions
+// Initially they had a const equal to the Client class.  This is related to old style React components built by class vs built by functions
 // The Client object prob has a render function in it, so it must be delcared this way wiht App as a const assigned to it rather than declaring App as a function.
 // YES, it is class.  Console.log App to see what Client returned!
 // As you can see below, I also tried a variant.  Assigned Client() return to a separate var, Game.  Then in the App() function, I rendered the <Game /> component.  That works.
 
 // const App = Client({game: TicTacToe});
 
-const Game = Client({game: TicTacToe, board: TicTacToeBoard,});
+const Game = Client({game: TicTacToe, board: TicTacToeBoard, numPlayers: 2});
 console.log("contents of Game: ", Game);
 
 function App() {
--- bgio-tutorial/src/Game.js
+++ bgio-tutorial/src/Game.js
@@ -1,4 +1,5 @@
 import {INVALID_MOVE} from 'boardgame.io/core';
+import { TurnOrder } from 'boardgame.io/core';
 
 
 function IsVictory(cells) {
@@ -31,18 +32,68 @@ function IsDraw(cells) {
     return cells.filter(c => c === null).length === 0;
 }
 
+// The suits of cards
+const suits = ["Clubs", "Spades", "Hearts", "Diamonds"];
+
+// Represent cards. 1 = Ace, 11-13 = jack, queen, king
+const ranks = [1,2,3,4,5,6,7,8,9,10,11,12,13];
+
+// returns an array of cards that will be shoe/deck games are played with
+// numDecks = Number of decks to use (a typical blackjack deck incl 6-8 decks of cards)
+// If numDecks is not provided or not a valid Int (int >= 1), then will set to 1
+function buildDeck(numDecks = 1) {
+    if (Number.isNaN(numDecks) || !Number.isInteger(numDecks) || numDecks < 1) {
+        numDecks = 1;
+    }
+    let theDeck = []
+    for (let d = 0; d < numDecks; d++) {
+        for (let s = 0; s < suits.length; s++) {
+            for (let r = 0; r < ranks.length; r++) {
+                theDeck.push({
+                    rank: ranks[r],
+                    suit: suits[s]
+                })
+            }
+        }
+    }
+    // console.log("here is the freshly built deck: ", theDeck);
+    return theDeck;
+}
+
+// // NOT NEEDED. Can do this directly in setup with array methods
+// function initBanks(numPlayers){ 
+//     let banks = [];
+//     for (let i=0; i<numPlayers; i++){
+//         banks[i] = 1000;
+//     }
+//     console.log("this is content of banks: ", banks);
+//     return banks;
+// }
 
 
 export const TicTacToe = {
-    setup: () => (
+    setup: ({G, ctx}) => (
         {
-        cells: Array(9).fill(null), 
-        quit: null
+            cells: Array(9).fill(null), 
+            quit: null,
+            gameSettings: {
+                numDecks: 2,
+                },
+            deck: [],
+            playerHands: [],
+            playerBanks: Array(ctx.numPlayers).fill(1000), // initBanks(ctx.numPlayers),
+            playerBets: Array(ctx.numPlayers).fill(0),
+
         }
-        ),
+    ),
+
 
     moves: {
-        clickCell: ({G, playerID}, id) => {
+        clickCell: ({G, playerID, ctx}, id) => {
+            console.log("here is playOrder[]: ", ctx.playOrder);
+            console.log("here is G: ", JSON.stringify(G));
+            // console.log("cards in deck: ", G.deck.length);
+            console.log("here is ctx: ", ctx);
             if (G.cells[id] !== null) {
                 return INVALID_MOVE;
             }
@@ -55,22 +106,75 @@ export const TicTacToe = {
         }
     },
 
-    turn: {
-        minMoves: 1,
-        maxMoves: 1
+    phases: {
+        betting: {
+            onBegin: ({G, random, ctx}) => {
+                // reset the deck and shuffle
+                G.deck = random.Shuffle(buildDeck(G.gameSettings.numDecks));
+                // console.log("in onbegin of betting phase. Here is shuffled g deck: ",JSON.stringify(G.deck));
+                console.log("here is value of ctx in onbegin betting: ", ctx);
+                // initialize the player hands array
+                for (let i = 0; i< ctx.numPlayers; i++) {
+                    G.playerHands[i] = [];
+                    G.playerBets[i] = 0;
+                }
+            },
+            start: true,
+            turn: {
+                minMoves: 1,
+                maxMoves: 1,
+            },
+            moves: {
+                bet: ({G, playerID}, betAmt) => {
+                    if (!betAmt || Number.isNaN(betAmt) || !Number.isInteger(betAmt) || betAmt < 1 || betAmt > G.playerBanks[playerID]) {
+                        return INVALID_MOVE;
+                    }
+                    G.playerBets[playerID] = betAmt;
+                    G.playerBanks[playerID] -= betAmt;
+                }
+            },
+            endIf: ({G}) => {
+                return G.playerBets.every((bet) => {return bet > 0});
+            },
+            next: 'playing',
+        }, 
+
+        playing: {
+            moves: {
+                hit: ({G, playerID}) => {
+                    console.log("here are the cards left in the deck before hit: ", G.deck.length);
+                    console.log("here is playerID from hit move: ", playerID);
+                    G.playerHands[playerID].push(G.deck.pop());
+                    // G.deck.pop();
+                    console.log("here's the player's hand: ", JSON.stringify(G.playerHands[playerID]));
+                    console.log("here are the cards left in the deck after hit: ", G.deck.length);
+                },
+                stand: ({events}) => {
+                    events.endTurn();
+                },
+            },
+            turn: {
+                order: TurnOrder.RESET,
+            },
+        },
     },
+
+    // turn: {
+    //     minMoves: 1,
+    //     maxMoves: 1
+    // },
 
     
     endIf: ({G, ctx}) => {
-        if (IsVictory(G.cells)) {
-            // You know it's the current player because this endIf check is run after every state change (ie, move).  So if it found winner then it must have occurred during currentPlayer's move
-            console.log("endif isVict check ran");
-            return { winner: ctx.currentPlayer};
-        }
-        if (IsDraw(G.cells)) {
-            console.log("endif isdraw check ran");
-            return { draw:true };
-        }
+        // if (IsVictory(G.cells)) {
+        //     // You know it's the current player because this endIf check is run after every state change (ie, move).  So if it found winner then it must have occurred during currentPlayer's move
+        //     console.log("endif isVict check ran");
+        //     return { winner: ctx.currentPlayer};
+        // }
+        // if (IsDraw(G.cells)) {
+        //     console.log("endif isdraw check ran");
+        //     return { draw:true };
+        // }
         if (G.quit === true) {
             alert("game over");
             console.log("endif quit check ran");
