b02358afb169288b3b7dd49740f8fae037cd6728
Kevin Ip <ipkevin@gmail.com>
2023-04-08 01:36:18 -0700

    simplify and finish custom CSS to create scaling neon bar border on text

--- bgio-tutorial/src/Board.js
+++ bgio-tutorial/src/Board.js
@@ -61,16 +61,47 @@ export function BlackjackBoard({ ctx, G, moves }) {
         <div className="main">
             <div className="bgtable">
 
-            <div className="result__wrapper">
-            {/* <h3 className="result result-0">W</h3> */}
-            <h3 className="result result-1 result-1tl">W</h3>
-            <h3 className="result result-1 result-1tr">W</h3>
-            <h3 className="result result-1 result-1br">W</h3>
-            <h3 className="result result-1 result-1bl">W</h3>
-            <h3 className="result result-2">W</h3>
-            <h3 className="result result-3">W</h3>
+
+            <div className="result__wrapper-outer">
+                <div className="result__wrapper result__wrapper-w">
+                <h3 className="result result-0">W</h3>
+                <h3 className="result result-1">W</h3>
+                <h3 className="result result-2">W</h3>
+                <h3 className="result result-3">W</h3>
+                </div>
+                <div className="result__wrapper result__wrapper-i">
+                <h3 className="result result-0">I</h3>
+                <h3 className="result result-1">I</h3>
+                <h3 className="result result-2">I</h3>
+                <h3 className="result result-3">I</h3>
+                </div>
+                <div className="result__wrapper result__wrapper-n1">
+                <h3 className="result result-0">N</h3>
+                <h3 className="result result-1">N</h3>
+                <h3 className="result result-2">N</h3>
+                <h3 className="result result-3">N</h3>
+                </div>
+                <div className="result__wrapper result__wrapper-n2">
+                <h3 className="result result-0">N</h3>
+                <h3 className="result result-1">N</h3>
+                <h3 className="result result-2">N</h3>
+                <h3 className="result result-3">N</h3>
+                </div>
+                <div className="result__wrapper result__wrapper-e">
+                <h3 className="result result-0">E</h3>
+                <h3 className="result result-1">E</h3>
+                <h3 className="result result-2">E</h3>
+                <h3 className="result result-3">E</h3>
+                </div>
+                <div className="result__wrapper result__wrapper-r">
+                <h3 className="result result-0">R</h3>
+                <h3 className="result result-1">R</h3>
+                <h3 className="result result-2">R</h3>
+                <h3 className="result result-3">R</h3>
+                </div>
             </div>
 
+            
                 {/* <table id="board">
         <tbody>{tbody}</tbody>
         </table>
