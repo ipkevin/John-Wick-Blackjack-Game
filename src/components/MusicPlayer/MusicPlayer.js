bb3f46202dba94b3909eeeb9ebf7d54f22a59487
Kevin Ip <ipkevin@gmail.com>
2023-04-10 18:31:00 -0700

    remove unused code

--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -2,8 +2,6 @@ import {useRef, useEffect} from 'react';
 
 import './MusicPlayer.scss';
 import songBG from '../../assets/sounds/lecastlevania_ledspirals2.mp3';
-import audioOff from '../../assets/images/audio-icon-cross2.png';
-import audioOn from '../../assets/images/audio-icon-on.png';
 
 const playIcon = `<div class="music-player__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
 <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM18.584 5.106a.75.75 0 011.06 0c3.808 3.807 3.808 9.98 0 13.788a.75.75 0 11-1.06-1.06 8.25 8.25 0 000-11.668.75.75 0 010-1.06z" />
@@ -33,15 +31,12 @@ export default function MusicPlayer(){
         if (audio.current.paused) {
             audio.current.play();
             musicIcon.current.innerHTML = playIcon;
-            // e.target.innerHTML = pauseIcon;
         } else {
             audio.current.pause();
             musicIcon.current.innerHTML = pauseIcon;
-            // e.target.innerHTML = playIcon;
         }
     }
 
-    console.log("here is music icon's ref's current: ", musicIcon.current);
     return (
         <div className="music-player">
             <button onClick={handleMusicIconClick} className="std-btn music-player__button" ref={musicIcon}>{playIcon}</button>
