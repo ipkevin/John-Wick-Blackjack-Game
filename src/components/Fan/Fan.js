09bcd9b01a469665bf6bd784be71707f2f44f5ba
Kevin Ip <ipkevin@gmail.com>
2023-04-12 10:54:57 -0700

    fan button changes

--- src/components/Fan/Fan.js
+++ src/components/Fan/Fan.js
@@ -3,8 +3,10 @@ import './Fan.scss';
 
 import useSound from 'use-sound';
 import btnSound from "../../assets/sounds/btn_click_quiet.ogg";
-import fanIconOn from "../../assets/images/fanicon-on.png";
-import fanIconOff from "../../assets/images/fanicon-off.png";
+// import fanIconOn from "../../assets/images/fanicon-on.png";
+// import fanIconOff from "../../assets/images/fanicon-off.png";
+import fanIconOn from "../../assets/images/toggle-on.png";
+import fanIconOff from "../../assets/images/toggle-off.png";
 
 
 export default function Fan(){
@@ -36,7 +38,7 @@ export default function Fan(){
                 <div className="fan__blade fan__blade-cross"></div>
             </div>
             <div className="fan__control">
-                <button className="std-btn fan__button" onClick={toggleFan}><img className="fan__button-image" src={(fanStatus === "") ? fanIconOn : fanIconOff} alt="fan on" /></button>
+                <button className="std-btn fan__button" onClick={toggleFan}>Fan <img className="fan__button-image" src={(fanStatus === "") ? fanIconOn : fanIconOff} alt="fan on" /></button>
             </div>
         </>
     );
